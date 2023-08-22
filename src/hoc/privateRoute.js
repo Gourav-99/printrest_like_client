1213dd95aef77c333a0f994a043abbb126201884
Gourav <goupg1999@gmail.com>
2023-08-22 12:40:11 +0530

    checking oauth

--- src/hoc/privateRoute.js
+++ src/hoc/privateRoute.js
@@ -4,6 +4,7 @@ import { Navigate } from "react-router";
 const privateRoute = (Component) => {
   const Route = (props) => {
     const auth = useSelector((state) => state.auth);
+    console.log("private route", auth);
     return auth.token ? <Component {...props} /> : <Navigate to="/login" />;
   };
   return Route;
